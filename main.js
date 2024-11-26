5f860571414acecb72dea9e89fb7712f6c71df8b
SvitlanaTurych bc2024-5 <svitlana.study24@gmail.com>
2024-11-26 14:38:19 +0200

    GET /notes/<ім’я нотатки>

--- main.js
+++ main.js
@@ -1,25 +1,29 @@
-const exprerss = require('express');
-const { Command } = require('commander');
+const express = require('express');
 const fs = require('fs');
-
-const app = exprerss();
+const path = require('path');
+const { Command } = require('commander');
 const program = new Command();
-
+const multer = require ('multer');
+const mmmm = multer()
 program
   .requiredOption('-h, --host <host>', 'server host')
   .requiredOption('-p, --port <port>', 'server port')
-  .requiredOption('-c, --cache <cache>', 'path to cache directory');
+  .requiredOption('-c, --cache <path>', 'cache directory');
 
 program.parse(process.argv);
-
 const options = program.opts();
 
-if (!fs.existsSync(options.cache)) {
-  console.error(`Error: Cache directory "${options.cache}" does not exist.`);
-  process.exit(1);
-}
-app.get('/', (req, res) => {
-  res.send('Сервер працює!');
+const app = express();
+app.use(express.json());
+
+
+app.get('/notes/:name', (req, res) => {
+  const notePath = path.join(options.cache, `${req.params.name}.txt`);
+  if (!fs.existsSync(notePath)) {
+    return res.status(404).send('Not found');
+  }
+  const noteText = fs.readFileSync(notePath, 'utf-8');
+  res.send(noteText);
 });
 
 app.listen(options.port, options.host, () => {
